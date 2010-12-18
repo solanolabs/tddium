ded88a719576f2d1cabb6b850136c75afde657be
Jay Moorthi <jaymoorthi@me.com>
2010-12-17 16:52:34 -0800

    Initial commit to tddium.

---
+++ test/test_tddium.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestTddium < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
