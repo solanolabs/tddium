27686a7365438265305d5bba9cf036f06d2d0b94
East Agile <developers@eastagile.com>
2011-03-16 13:49:27 +0700

    Remove unneccessary dependencies in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,13 +3,5 @@ Copyright (c) 2011 Solano Labs All Rights Reserved
 =end
 
 require "tddium"
-require "fakeweb"
-require "rack/test"
 require "fakefs/spec_helpers"
 require "tddium_client/tddium_spec_helpers"
-
-FakeWeb.allow_net_connect = false
-
-def fixture_path(fixture_name)
-  File.join File.dirname(__FILE__), "fixtures", fixture_name
-end
