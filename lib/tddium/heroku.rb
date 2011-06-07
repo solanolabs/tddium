c3b10d72032adb5569692af2c60ead485d0aa740
Jay Moorthi <jay@tddium.com>
2011-06-07 13:40:00 -0700

    fix missing variable detection

--- lib/tddium/heroku.rb
+++ lib/tddium/heroku.rb
@@ -2,15 +2,23 @@
 Copyright (c) 2011 Solano Labs All Rights Reserved
 =end
 
+require 'timeout'
+
 class HerokuConfig
   REQUIRED_KEYS = %w{TDDIUM_API_KEY TDDIUM_USER_NAME}
   def self.read_config(app=nil)
     result = nil
     begin
       config = {}
+      
       command = "heroku config -s"
       command += " --app #{app}" if app
-      output = `#{command}`
+
+      output = ''
+      Timeout::timeout(5) {
+        output = `#{command}`
+      }
+
       output.lines.each do |line|
         line.chomp!
         k, v = line.split('=')
@@ -18,7 +26,7 @@ class HerokuConfig
           config[k] = v
         end
       end
-      return nil if REQUIRED_KEYS.inject(false) {|missing, x| !config[x]}
+      return nil if REQUIRED_KEYS.inject(false) {|missing, x| missing || !config[x]}
       result = config if config.keys.length > 0
     rescue 
     end
