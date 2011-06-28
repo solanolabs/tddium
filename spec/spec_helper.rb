c923204a11529649f99e780ddd68d920ae9278ad
William Josephson <wkj@tddium.com>
2011-06-28 16:52:46 -0400

    Issue a warning for uncommitted changes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,16 @@ require "tddium"
 require 'rspec'
 require "fakefs/spec_helpers"
 require "tddium_client/tddium_spec_helpers"
+
+require 'ostruct'
+
+class Open3SpecHelper
+  def self.stubOpen2e(data, ok, block)
+    stdin = StringIO.new
+    output = StringIO.new(data)
+    status = (ok && 0) || 1
+    value = OpenStruct.new(:exitstatus => status, :to_i => status)
+    wait = OpenStruct.new(:value => value)
+    block.call(stdin, output, wait)
+  end
+end
