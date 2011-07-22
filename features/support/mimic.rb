78c9b3e339c66fcb1d3a7eeaa92aa993f716c4d8
William Josephson <wkj@tddium.com>
2011-07-22 02:23:49 -0400

    Wrap mimic in a simple server.

--- features/support/mimic.rb
+++ features/support/mimic.rb
@@ -1,7 +1,30 @@
 require 'mimic'
 require 'daemons'
 
-Daemons.run_proc("mimic") do
-  Mimic.mimic({:fork => false, :host => 'localhost', :port => 8080, :remote_configuration_path => '/api'}) do
+class MimicServer
+  attr_reader :port
+
+  def initialize
+    @port = 8080
+    @pid_dir = '/tmp/mimic'
+    FileUtils.mkdir_p('/tmp/mimic')
+  end
+
+  def start
+    options = {:ARGV => ['start'], :dir_mode => :normal, :dir => @pid_dir}
+    args = {:fork => false,
+            :host => 'localhost',
+            :port => @port,
+            :remote_configuration_path => '/api'}
+    @mimic_group = Daemons.run_proc("mimic", options) do
+      Mimic.mimic(args) do
+      end
+    end
+  end
+
+  def stop
+    @mimic_group.stop_all
+    @mimic_group.find_applications(@pid_dir)
+    @mimic_group.zap_all
   end
 end
