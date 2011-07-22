42a75884357ac69051584d1f138f035f2d1df02b
William Josephson <wkj@tddium.com>
2011-07-22 03:19:37 -0400

    Wrap Mimic REST API.

--- features/support/mimic.rb
+++ features/support/mimic.rb
@@ -1,5 +1,7 @@
 require 'mimic'
+require 'timeout'
 require 'daemons'
+require 'httparty'
 
 class MimicServer
   attr_reader :port
@@ -18,8 +20,11 @@ class MimicServer
             :remote_configuration_path => '/api'}
     @mimic_group = Daemons.run_proc("mimic", options) do
       Mimic.mimic(args) do
+        [:INT, :TERM].each { |sig| trap(sig) { Kernel.exit!(0) } }
       end
     end
+    @mimic_group.setup
+    wait
   end
 
   def stop
@@ -27,4 +32,58 @@ class MimicServer
     @mimic_group.find_applications(@pid_dir)
     @mimic_group.zap_all
   end
+
+  def wait
+    (0...5).each do |i|
+      if ping then
+        break
+      end
+      Kernel.sleep(0.1)
+    end
+  end
+
+  def ping
+    begin
+      http = call_api(:get,  '/api/ping')
+    rescue Exception
+      return false
+    end
+    return http.code == 200
+  end
+
+  def clear
+    http = call_api(:post,  '/api/clear')
+    return http
+  end
+
+  def install(verb, path, body, headers = {})
+    params = { 'path' => path, 'body' => body }
+    http = call_api(:post,  "/api/#{verb}", params, headers)
+    return http
+  end
+
+  def call_api(method, path, params = {}, headers = {})
+    tries = 0
+    retries = 5
+    done = false
+    while (tries <= retries) && !done
+      begin
+        http = HTTParty.send(method, "http://localhost:#{@port}#{path}",
+                             :body => params, :headers => headers)
+        done = true
+      rescue Timeout::Error
+      ensure
+        tries += 1
+      end
+    end
+    raise Timeout::Error if tries > retries && retries >= 0
+    return http
+  end
+end
+
+if __FILE__ == $0 then
+  mimic = MimicServer.new
+  mimic.start
+  mimic.clear
+  mimic.stop
 end
