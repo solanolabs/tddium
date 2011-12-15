54745c67e425413f6ad39322ce4e75d1bdd21c8d
Jay Moorthi <jay@tddium.com>
2011-12-15 10:17:58 -0800

    clean up dup steps

--- features/step_definitions/aruba_steps.rb
+++ features/step_definitions/aruba_steps.rb
@@ -13,10 +13,3 @@ Then /^the output from "([^"]*)" should contain:$/ do |cmd, expected|
   assert_partial_output(expected, output_from(cmd))
 end
 
-Then /^the output from "([^"]*)" should not contain "([^"]*)"$/ do |cmd, unexpected|
-  assert_no_partial_output(unexpected, output_from(cmd))
-end
-
-Then /^the output should not contain "([^"]*)"$/ do |unexpected|
-  assert_no_partial_output(unexpected, all_output)
-end
