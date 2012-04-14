009fc9cf76b0f49afdb0407d11b3a3ebc436d4a3
William Josephson <wkj@tddium.com>
2012-04-14 15:12:02 -0400

    Clearer spelling of require directives.

--- lib/tddium.rb
+++ lib/tddium.rb
@@ -1,8 +1,8 @@
 # Copyright (c) 2011, 2012 Solano Labs All Rights Reserved
 
-require File.expand_path("../tddium/constant", __FILE__)
-require File.expand_path("../tddium/version", __FILE__)
-require File.expand_path("../tddium/heroku", __FILE__)
+require File.expand_path(File.join(File.dirname(__FILE__), "tddium/constant"))
+require File.expand_path(File.join(File.dirname(__FILE__), "tddium/version"))
+require File.expand_path(File.join(File.dirname(__FILE__), "tddium/heroku"))
 
 module Tddium
   class TddiumError < Exception
