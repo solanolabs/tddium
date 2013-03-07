1bec067faf7939029d599ca632e673f3004fec74
William Josephson <wkj@tddium.com>
2013-03-06 23:21:16 -0500

    Bump release version.

--- lib/tddium/version.rb
+++ lib/tddium/version.rb
@@ -1,5 +1,5 @@
-# Copyright (c) 2011, 2012 Solano Labs All Rights Reserved
+# Copyright (c) 2011, 2012, 2013 Solano Labs All Rights Reserved
 
 module Tddium
-  VERSION = "1.7.3"
+  VERSION = "1.7.4"
 end
